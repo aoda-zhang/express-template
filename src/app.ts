6c4b412467947b2e75f91702b62b7a517bbd3ae3
aodaGit <aoda2826@qq.com>
2023-03-01 20:56:25 +0800

    更新部分配置

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,10 @@ import initMiddleware from './middleware'
 import initRouter from './routes'
 // import connectDataBase from './middleware/dataBaseConnect'
 const app = express()
-// 加载环境变量
-renderENVConfig()
 // 加载路由
 initRouter(app)
+// 加载环境变量
+renderENVConfig()
 // 挂载中间件
 initMiddleware(app)
 // 数据库连接
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,20 +3,15 @@ import goodsRouters from '../modules/goods/api'
 import loginRoute from '../modules/login/route'
 import usersRouters from '../modules/users/route'
 // 无token路由
-export const noTokenRouters = ['/login', '/welcome']
+export const noTokenRouters = ['/login']
 const initRouter = (app: Express) => {
   const PREFIX = process?.env?.PREFIX ?? '/api'
-  const path = require('path')
   const routeRender: Router[] = [loginRoute, goodsRouters, usersRouters]
-  app.use(express.static(path.join(__dirname, 'public')))
-  app.get('/', (req, res) => {
-    res.send('默认页面')
-  })
-  // app.use(express.static('/public'))
+  // 前端静态资源加载
+  app.use(express.static('public'))
   // 全局路由前缀+路由中间件
-
   app.use(PREFIX, routeRender)
-  app.use('*', async (req, res) => {
+  app.use('*', (req, res) => {
     // 跳转默认路由
     res.send('默认页面')
   })
