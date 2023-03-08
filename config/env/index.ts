dbb254c6914e8547b874727157b877b4fcd24fbd
aodaGit <aoda2826@qq.com>
2023-03-08 22:11:21 +0800

    去掉vercel部署

--- config/env/index.ts
+++ config/env/index.ts
@@ -4,8 +4,12 @@ dotenv.config({
   path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
 })
 export default {
+  // 运行端口
   PORT: process.env.PORT,
+  // 加密key
   SCRECT_KEY: process.env.SCRECT_KEY,
+  // 数据库连接
   DATABASE_URL: process.env.DATABASE_URL,
+  // 全局API前缀
   PREFIX: process.env.DATABASE_URL
 }
