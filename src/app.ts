c7ef67cd615992f9fea748500a7f0c723dd5e94e
aodaGit <aoda2826@qq.com>
2023-03-24 18:59:13 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,28 @@ import env from '../config/env'
 import initMiddleware from './middleware'
 import initRouter from './routes'
 import DBConnection from './utils/DBConnection'
+import helmet from 'helmet'
+import cors from 'cors'
+import rateLimit from 'express-rate-limit'
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers
+})
+
 const app = express()
+// 安全设置
+app.use(helmet())
+// prox 代理
+app.use(cors())
+// 路由注册
 initRouter(app)
+// 中间件注册
 initMiddleware(app)
+// 限速设置
+app.use(apiLimiter)
+// 数据库连接
 DBConnection()
 
 const PORT = env?.PORT ?? 8080
