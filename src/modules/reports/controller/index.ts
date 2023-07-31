935a832f57160237ee2aa33f4527cf803384ecd4
aodaGit <aoda2826@qq.com>
2023-07-31 22:38:11 +0800

    更新文件处理操作模块

--- src/modules/reports/controller/index.ts
+++ src/modules/reports/controller/index.ts
@@ -7,7 +7,7 @@ class Report {
       // 1.使用path.resolve来处理文件路径
       // 2.注意存放文件的相对地址
       res.download(
-        path.resolve(__dirname, '/../../../../assets/files/night.jpg')
+        path.resolve(__dirname, '../../../../assets/files/night.jpg')
       )
     } catch (error) {
       res.status(400).json('文件获取失败')
